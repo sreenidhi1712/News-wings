241ddc74713f96c6b51c69af62f1839a87fe930c
[Sreenidhi D] <[sreeindra01@gmail.com]>
2024-06-19 13:59:52 +0530

    updated card structure

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,10 +7,10 @@ import { FaFire } from "react-icons/fa";
 function Footer() {
   return (
     <div className='bottom-0 left-0 w-screen h-10 bg-green-500 flex items-center justify-evenly'>
-        <IoMdHome className='h-6 w-6 text-white'/>
+        <IoMdHome className='h-7 w-7 text-white'/>
         <FaBookmark  className='h-6 w-6 text-white'/>
         <FaFire className='h-6 w-6 text-white'/>
-        <IoIosSearch className='h-6 w-6 text-white'/>
+        <IoIosSearch className='h-7 w-7 text-white'/>
     </div>
   )
 }
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useEffect ,useState} from 'react'
 import './Style.css'
+import axios from 'axios';
+
 
 
 function Homepage() {
 
+    const [news, setNews] = useState([]);
+ 
+    useEffect(()=>{
+        const fetchdata = async ()=>{
+            const response = await axios.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=4c8372e1b7fa43c9a89c2a176b9461bb&page=1&pagesize=3')
+            setNews(response.data.articles)
+        }
+        fetchdata();
+    },[])
    
   return (
     <div className='w-screen '>
-{/* news cards */}
+{/* news cards  https://newsapi.org/v2/top-headlines?country=us&apiKey=4c8372e1b7fa43c9a89c2a176b9461bb */}
            <div className='flex overflow-x-scroll hide-scrollbar w-full  mt-28   '> 
-               {Array(3).fill(0).map(()=>(
-                        <div className='h-36 w-[85%] mx-3 bg-slate-200 flex-shrink-0 rounded-lg'>
-                                 
+               {news.map((items)=>(
+                        <div className={`h-36 w-[85%] mx-3 flex  flex-shrink-0 rounded-lg  bg-cover bg-center`} style={{ backgroundImage: `url(${items.urlToImage})` }}>
+                                 <p className='font-bold text-sm text-slate-200 mt-14 ml-3'>{items.title}</p>
                         </div>
                      )
                )}
            </div>
 
-           <Cards title={"Trending Topic"}/>
+
+           <div className='flex flex-col w-full mt-5'>
+               <p className='mx-3 font-bold text-xl'>Trending Topic</p>
+               <div className={`flex overflow-x-scroll hide-scrollbar w-full `} >
+               {Array(3).fill(0).map(()=>(
+                        <div className={`h-32 w-[40%] mt-5 mx-3 flex-shrink-0  bg-slate-200  rounded-lg`}>
+                                 
+                        </div>
+                     )
+               )}
+               </div>
+           </div>
 
            <Cards title={"Headlines"}/>
 
@@ -38,9 +60,9 @@ const Cards = ({title})=>{
      {/* headlines */} 
     <div className='flex flex-col w-full mt-5'>
                <p className='mx-3 font-bold text-xl'>{title}</p>
-               <div className={`${title === "Trending Topic"?`flex overflow-x-scroll hide-scrollbar w-full `:`flex flex-col w-full items-center `}`} >
+               <div className={`flex flex-col w-full items-center `} >
                {Array(3).fill(0).map(()=>(
-                        <div className={`${title === "Trending Topic"?`h-32 w-[40%] mt-5 mx-3 flex-shrink-0`:  `h-32 w-[90%]  my-2`}  bg-slate-200  rounded-lg`}>
+                        <div className={  `h-32 w-[90%]  my-2 bg-slate-200  rounded-lg`}>
                                  
                         </div>
                      )
